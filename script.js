4e85ef7f6772622f56dc91a59298dcb92116765d
Mukesh Gehlot <omuksa007@gmail.com>
2022-03-29 13:39:20 +0530

    change in date

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursE1 = document.getElementById("hours");
 const minsE1 = document.getElementById("mins");
 const secondsE1 = document.getElementById("seconds");
 
-const newYears = "1 Jan 2022";
+const newYears = "1 Jan 2023";
 
 function countdown() {
   const newYearsDate = new Date(newYears);
